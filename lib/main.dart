8e9cf47ce4b9ebd13d4f1ccb624d73844adb60bf
Rabra <rabira.hierpa@gmail.com>
2020-03-20 12:01:11 +0300

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+
+void main() => runApp(MaterialApp(home: HomeScreen()));
+
+class HomeScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Flutter Beginner'),
+        centerTitle: true,
+        backgroundColor: Colors.indigo[900],
+      ),
+      body: Container(
+        child: Center(
+          child:Image.asset('assets/img/space-1.jpg'),
+          //child: Imange.network('https://www.splash.com/space-backgroudn.png')
+        ),
+        padding: EdgeInsets.all(0.0),
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.add, size: 24),
+        backgroundColor: Colors.red[900],
+        onPressed: () {
+          print('Button Clicked');
+        },
+        tooltip: 'Add',
+      ),
+    );
+  }
+}
